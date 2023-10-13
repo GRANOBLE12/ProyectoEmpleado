8204e406ed5907bc814432b91f6dff32a3d113ba
ANDRES GARCES <ANDRES GARCES@169.254.45.111>
2023-10-13 07:56:26 -0500

    Commit

--- src/main/java/Logica_Cliente/Main.java
+++ src/main/java/Logica_Cliente/Main.java
@@ -14,6 +14,8 @@ import java.util.Scanner;
  */
 public class Main {
 
+    private static int i;
+
     /**
      * @param args the command line arguments
      */
@@ -23,11 +25,12 @@ public class Main {
 //Declarar Variables
     String nombre = "";
     String codigo = "";
-    String ID = "";
+    String Id = "";
     int año_ingreso = 0;
     int cantidad = 0;
     int opcion = 0;
     int bandera = 0 ;
+    retorno
    
 //Creación de Objetos
     Empleado objempleado = new Empleado();
@@ -36,7 +39,7 @@ public class Main {
     Scanner scan = new Scanner(System.in);
     
 //Almacenar Datos en Memoria
-    ArrayList < Empleado> listaempleados = new ArrayList<>();
+    ArrayList < Empleado > listaempleados = new ArrayList <> ();
 do {
     
 //Impresión del Menú de Opciones
@@ -44,7 +47,7 @@ do {
     System.out.println("Menú de Opciones:");
     System.out.println("1. Registrar empleado(s)");
     System.out.println("2. Editar empleado(s)");
-    System.out.println("3. Consultar empleado(s) ");
+    System.out.println("3. Buscar empleado(s) ");
     System.out.println("4. Mostrar empleado(s)");
     System.out.println("5. Eliminar empleado(s)");
     System.out.println("6. Salir del programa"); 
@@ -58,42 +61,79 @@ do {
     case 1:   
         
 //Registro Empleado(s) 
-                                   System.out.println(""); //Para dar espacio
+                                  System.out.println(""); //Para dar espacio
 System.out.println("Digite la cantidad de empleados a registrar");
     cantidad = scan.nextInt();
     scan.nextLine();
     
-//Ciclo para                 
-    for (int i = 0; i < cantidad; i++) {
+//Ciclo para 
+    if (cantidad > 0) {
+        
+    for (int i = 0; i < cantidad ; i++)
+    {    
                                    System.out.println(""); //Para dar espacio
-    System.out.println("Digite el nombre del empleado");
+    System.out.println("Digite el nombre del empleado " + (i + 1));
     nombre = scan.nextLine();
-    System.out.println("Digite el código del empleado");
-    codigo = scan.nextLine();
-    System.out.println("Digite el año de ingreso del empleado");
-    año_ingreso = scan.nextInt();
+    while(nombre.equals(""))
+    {        
+        System.out.println("Digite nuevamente el nombre del empleado");
+        nombre = scan.nextLine();
+        
+        retorno = RetornarValor(Nombre);
+    }    
     
+        retorno = RetornarValor(Nombre);
+        while (i )
+    
+                                   System.out.println(""); //Para dar espacio
+    System.out.println("Digite el código del empleado " + (i + 1));
+    codigo = scan.nextLine();
+    while(codigo.equals(""))
+    {                                        
+    System.out.println("Digite nuevamente el código del empleado");
+    codigo = scan.nextLine();
+    }    
+                                   System.out.println(""); //Para dar espacio
+    System.out.println("Digite el año de ingreso del empleado " + (i + 1));
+    año_ingreso = scan.nextInt();
+    while (año_ingreso < 1919 || año_ingreso >= 2023)
+   {                         
+                                   System.out.println(""); //Para dar espacio
+        System.out.println("El año de ingreso no es válido");
+        System.out.println("Digite nuevamente el año de ingreso del empleado");
+    año_ingreso = scan.nextInt();   
+    }
+     
     scan.nextLine();
     objempleado = new Empleado(nombre, año_ingreso, codigo);
     listaempleados.add(objempleado);
-}  
+    }
                                    System.out.println(""); //Para dar espacio
     System.out.println("El registro de empleado(s) ha sido exitoso");
+    
+    }else  {
+                                   System.out.println(""); //Para dar espacio
+   System.out.println("El número digitado no es válido");
+           
+    }
 break;
 
 case 2: 
     //Editar Empleado(s)
 scan.nextLine();
                                    System.out.println(""); //Para dar espacio
-    System.out.println("Digite el ID del empleado a modificar");
-    ID = scan.nextLine();
+    System.out.println("Digite el ID del empleado a editar");
+    codigo = scan.nextLine();
     for (int i = 0; i < listaempleados.size(); i++) {
-        if(listaempleados.get(i).getCodigo().equals(ID))
-{                                  System.out.println(""); //Para dar espacio
+        if(listaempleados.get(i).getCodigo().equals(codigo))
+{                                  
+                                   System.out.println(""); //Para dar espacio
         System.out.println("Digite el nombre del empleado");
         nombre = scan.nextLine();
+                                   System.out.println(""); //Para dar espacio
         System.out.println("Digite el código del empleado");
         codigo = scan.nextLine();
+                                   System.out.println(""); //Para dar espacio
         System.out.println("Digite el año de ingreso del empleado");
         año_ingreso = scan.nextInt();
                            
@@ -101,14 +141,13 @@ scan.nextLine();
         listaempleados.get(i).setCodigo(codigo);
         listaempleados.get(i).setAño_ingreso(año_ingreso);
                                    System.out.println(""); //Para dar espacio
-        System.out.println("El empleado "+ (i + 1) +" ha sido modificado");
+        System.out.println("El empleado "+ (i + 1) +" ha sido editado");
         bandera = 1;
 }   
     }
     if (bandera == 0) {
                                    System.out.println(""); //Para dar espacio    
-        System.out.println("El ID del empleado es incorrecto o no esta registrado");
-    
+        System.out.println("El ID del empleado es incorrecto o no está registrado");    
     }
  break;
  
@@ -117,12 +156,12 @@ case 3:
 scan.nextLine();
                                    System.out.println(""); //Para dar espacio
 System.out.println("Digite el código del empleado a buscar");
-    ID = scan.nextLine();
+    Id = scan.nextLine();
     for (int i = 0; i < listaempleados.size(); i++) {
-        if(listaempleados.get(i).getCodigo().equals(ID))
+        if(listaempleados.get(i).getCodigo().equals(Id))
         {
                                    System.out.println(""); //Para dar espacio
-        System.out.println("Empleado" + "\t" + (i + 1));
+        System.out.println("Empleado" + "\t" + (i + 1) + "\t" + "Ha sido buscado");
         System.out.println("El nombre del empleado es:" + "\t" + listaempleados.get(i).getNombre());
         System.out.println("El código del empleado es:" + "\t" + listaempleados.get(i).getCodigo());
         System.out.println("El año de ingreso del empleado es:" + "\t" + listaempleados.get(i).getAño_ingreso());
@@ -135,11 +174,12 @@ if (bandera == 0) {
                                    System.out.println(""); //Para dar espacio
     System.out.println("El ID del empleado es incorrecto o no está registrado");   
 }
-
 break;
 
 case 4:
 //Listar Empleado(s)
+                                   System.out.println(""); //Para dar espacio
+    System.out.println("Listado de los Empleados Registrados:");
 if (listaempleados.size() > 0) {
 for (int i = 0; i < listaempleados.size(); i++) {
                                    System.out.println(""); //Para dar espacio
@@ -159,12 +199,13 @@ case 5:
 scan.nextLine();
                                    System.out.println(""); //Para dar espacio
 System.out.println("Digite el código del empleado a eliminar");
-ID = scan.nextLine();
+Id = scan.nextLine();
 
 for (int i = 0; i < listaempleados.size(); i++) {
-    if(listaempleados.get(i).getCodigo().equals(ID))
+    if(listaempleados.get(i).getCodigo().equals(Id))
     {
                                    System.out.println(""); //Para dar espacio
+    listaempleados.remove(i);
     System.out.println("El Empleado "+ (i + 1) +" ha sido eliminado");
     bandera = 1;
     break;
@@ -186,5 +227,23 @@ break;
             System.out.println("Opción no válida");
             }
         }while(opcion != 6);
+    }
+    public static int RetornarValor(String nombre)
+    {
+        int letra = 0;
+        int número = 0;
+        
+        for (int i = 0; i < nombre.length(); i++){
+            boolean bandera = Character.isDigit(nombre.charAt(i));
+            if (bandera )
+            {
+                System.out.println("Es un número " + nombre.charAt(i));
+                número++;
+            } else {
+                System.out,println()
+            }
+        }
+        
+        return 0;
     }
 }
